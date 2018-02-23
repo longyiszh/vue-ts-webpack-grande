788e9c6303643ed80c503f512aaa1d03e18384d9
longyiszh <longyiszh@gmail.com>
2018-02-23 16:24:50 +0800

    + basic structure

---
+++ template/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <section id="app">
+    
+    <main class="router">
+      <router-view></router-view>
+    </main>
+
+  </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  name: 'app'
+})
+export default class App extends Vue {
+  
+}
+</script>
---
+++ template/src/main.ts
@@ -0,0 +1,23 @@
+// global styles
+import './styles.scss';
+
+import './declarations';
+
+import Vue from 'vue';
+import App from './App.vue';
+
+import VueRouter from "vue-router";
+
+import { appRoute } from "./app.route";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: appRoute
+});
+
+const v = new Vue({
+  el: '#app',
+  render: (h: any) => h(App),
+  router: router
+})
